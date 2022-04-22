8f0ef44f009405be40b307800a04ed1c96372e68
vishnupathak102 <vishnupathak1997@gmail.com>
2022-04-22 14:18:32 +0530

    twitch

---
+++ client/src/components/Modal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Poratl Creation for Modal
+const Modal = (props) => {
+    return ReactDOM.createPortal(
+        <div onClick={props.onDismiss} className='ui dimmer modals visible active'>
+            <div onClick={(e) => e.stopPropagation()} className='ui standard modal visible active'>
+                <div className='header'>{props.title}</div>
+                <div className='header'>
+                    {props.content}
+                </div>
+                <div className='actions'>{props.actions}   </div>
+            </div>
+        </div>,
+        document.querySelector("#modal")
+    );
+};
+
+export default Modal;
---
+++ client/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
+
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+
+// root.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
+
+
+
